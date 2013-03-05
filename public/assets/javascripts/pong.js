32f24cf8cd2c2bb52c52e070b9f45e8e094d1869
Tyler Heck <tyler.heck@gmail.com>
2013-03-04 21:51:30 -0500

    Add: functioning computer pong players

--- public/assets/javascripts/pong.js
+++ public/assets/javascripts/pong.js
@@ -1,14 +1,16 @@
 
-function PongPaddle (x, y, controllable, player_one) {
+function PongPaddle (x, y, ymin, ymax, controllable, player_one) {
 
   // PongPaddle CONSTANTS
-  this.Y_VELOCITY = 1; // normalized pixels per frame
+  this.Y_VELOCITY = 0.8; // normalized pixels per frame
   this.WIDTH = 2; // normalized pixels
   this.HEIGHT = 10; // normalized pixels
 
   // PongPaddle PROPERTIES
   this.x = x; // x position of the center of the paddle
   this.y = y; // y position of the center of the paddle
+  this.ymin = ymin;
+  this.ymax = ymax;
   this.controllabe = controllable; // boolean, is the paddle player 
                                    // controllable?
   this.player_one = player_one; // boolean, does the paddle represent player one
@@ -24,22 +26,25 @@ function PongPaddle (x, y, controllable, player_one) {
     }
     else {
       if ((this.y - y) > this.Y_VELOCITY)
-        this.y += this.Y_VELOCITY;
+        this.y -= this.Y_VELOCITY;
       else this.y = y;
     }
+
+    if(this.y - this.HEIGHT/2 < this.ymin) this.y = this.ymin + this.HEIGHT/2;
+    if(this.y + this.HEIGHT/2 > this.ymax) this.y = this.ymax - this.HEIGHT/2;
   };
 
   /* returns true if the y cordinate is within the bounds of the paddle, false 
    * if not */
   this.bounds = function (y) {
-    if (y < (this.y + this.WIDTH/2) && y > (this.y - this.WIDTH/2))
+    if (y < (this.y + this.HEIGHT/2) && y > (this.y - this.HEIGHT/2))
       return true;
     return false;
   };
 
   /* returns the speed modifier of a ball collision at y coordinate */
   this.smod = function (y) {
-    return (y - this.y)*2/this.WIDTH;
+    return (y - this.y)*2/this.HEIGHT;
   }
 
   /* draws the paddle on the screen with the given drawing context and modifiers
@@ -58,13 +63,14 @@ function PongBall (x, y) {
 
   // PongBall CONSTANTS
   this.SIZE = 2; // normalized pixels (side width)
-  this.DEFAULT_VELOCITY = 1; // normalized pixels per frame
-  this.MAX_VELOCITY = 3;
+  this.DEFAULT_X_VELOCITY = 1.5; // normalized pixels per frame
+  this.DEFAULT_Y_VELOCITY = 1; // normalized pixels per frame
+  this.MAX_VELOCITY = 2;
 
   // PongBall PROPERTIES
-  this.x_velocity = 1; // normalized pixels per frame
-  this.y_velocity = Math.random()*this.DEFAULT_VELOCITY*2 - 
-                    this.DEFAULT_VELOCITY; // normalized pixels per frame
+  this.x_velocity = this.DEFAULT_X_VELOCITY; // normalized pixels per frame
+  this.y_velocity = Math.random()*this.DEFAULT_Y_VELOCITY*2 - 
+                    this.DEFAULT_Y_VELOCITY; // normalized pixels per frame
   this.x = x; // normalized pixels
   this.y = y; // normalized pixels
   this.x_collision = false; // is ball mid-collision with an object in the x
@@ -131,17 +137,17 @@ function PongBall (x, y) {
     this.x = x;
     this.y = y;
 
-    if (x_direction == "left") this.x_velocity = -this.DEFAULT_VELOCITY;
-    else if (x_direction == "right") this.x_velocity = this.DEFAULT_VELOCITY;
+    if (x_direction == "left") this.x_velocity = -this.DEFAULT_X_VELOCITY;
+    else if (x_direction == "right") this.x_velocity = this.DEFAULT_X_VELOCITY;
     else {
-      if ((Math.random() - 0.5) < 0) this.x_velocity = -this.DEFAULT_VELOCITY;
-      else this.x_velocity = this.DEFAULT_VELOCITY;
+      if ((Math.random() - 0.5) < 0) this.x_velocity = -this.DEFAULT_X_VELOCITY;
+      else this.x_velocity = this.DEFAULT_X_VELOCITY;
     }
 
     /* give the ball some arbitrary y velocity between 0 and 
      * this.DEFAULT_VELOCITY */
-    this.y_velocity = Math.random()*this.DEFAULT_VELOCITY*2 - 
-                      this.DEFAULT_VELOCITY;
+    this.y_velocity = Math.random()*this.DEFAULT_Y_VELOCITY*2 - 
+                      this.DEFAULT_Y_VELOCITY;
   };
 
   /* draws the ball on the screen with the given drawing context and modifiers
@@ -175,37 +181,43 @@ function BoundingRange (min,max) {
 function Game (players) {
 
   // Game PROPERTIES
-  this.ball = new PongBall(50, 50);
+  this.ball = new PongBall(100, 50);
 
   if (players == 0) {
-    this.left_paddle = new PongPaddle(10,50,false,true);
-    this.right_paddle = new PongPaddle(90,50,false,false);
+    this.left_paddle = new PongPaddle(20,50,0,100,false,true);
+    this.right_paddle = new PongPaddle(179,50,0,100,false,false);
   }
   else if (players == 1) {
-    this.left_paddle = new PongPaddle(10,50,true,true);
-    this.right_paddle = new PongPaddle(90,50,false,false);
+    this.left_paddle = new PongPaddle(20,50,0,100,true,true);
+    this.right_paddle = new PongPaddle(179,50,0,100,false,false);
   }
   else {
-    this.left_paddle = new PongPaddle(10,50,true,true);
-    this.right_paddle = new PongPaddle(89,50,true,false);
+    this.left_paddle = new PongPaddle(20,50,0,100,true,true);
+    this.right_paddle = new PongPaddle(179,50,0,100,true,false);
   }
 
   this.left_gutter = new BoundingRange(-9,0);
-  this.right_gutter = new BoundingRange(99,109);
+  this.right_gutter = new BoundingRange(199,209);
   this.top_wall = new BoundingRange(-9,0);
   this.bottom_wall = new BoundingRange(99,109);
-  this.left_collision = new BoundingRange(10,10+Math.abs(this.ball.getXVelocity()+1));
-  this.right_collision = new BoundingRange(89-Math.abs(this.ball.getXVelocity()+1),89);
+  this.left_collision = new BoundingRange(20,20+Math.abs(this.ball.getXVelocity()+1));
+  this.right_collision = new BoundingRange(179-Math.abs(this.ball.getXVelocity()+1),179);
 
   // GAME METHODS
 
   this.draw = function (context) {
-    var xmod = $(window).width()/100;
+    var xmod = $(window).width()/200;
     var ymod = $(window).height()/100;
 
     this.ball.draw(context, 1, 1);
     this.left_paddle.draw(context, 1, 1);
     this.right_paddle.draw(context, 1, 1);
+    
+    context.moveTo(-0.5,-0.5);
+    context.lineTo(200.5,-0.5);
+    context.moveTo(-0.5,100.5);
+    context.lineTo(200.5,100.5);
+    context.stroke();
   };
 
   this.step = function () {
@@ -228,9 +240,9 @@ function Game (players) {
     }
 
     // check for passed balls (ADD POINT SCORING)
-    if (this.left_gutter.bounds(this.ball.getX())) this.ball.reset(50,50,"random");
+    if (this.left_gutter.bounds(this.ball.getX())) this.ball.reset(100,50,"right");
     else if (this.right_gutter.bounds(this.ball.getX())) 
-      this.ball.reset(50,50,"random");
+      this.ball.reset(100,50,"left");
 
     // MOVEMENT
     this.ball.move();
@@ -248,7 +260,6 @@ function Game (players) {
 
 
 $(function () {
-
   var game = new Game (0);
   var canvas = $('#canvas')[0];
 
