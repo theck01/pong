4407066f1a90f97172f0e89e38d725707b95317a
Tyler Heck <tyler.heck@gmail.com>
2013-03-11 13:04:42 -0400

    Convert app.rb from classic app to modular

--- app.rb
+++ app.rb
@@ -1,42 +1,52 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/config_file'
 require 'haml'
 require './lib/sinatra/partials'
 require './lib/sinatra/javascripts'
 require './lib/sinatra/styles'
 
-config_file 'config.yml'
+class PongApp < Sinatra::Base
+  register Sinatra::ConfigFile
+  helpers Sinatra::Partials
+  helpers Sinatra::Javascripts
+  helpers Sinatra::Styles
 
-# ROUTES
+  config_file 'config.yml'
 
-get '/' do
-  styles :application, :menus
-  scripts :application, :pong
-  @layout = { menu: 'menus/main' }
-  @num_players = 0
-  haml :main_page
-end
+  # ROUTES
 
-get '/single' do
-  styles :application, :menus
-  scripts :application, :pong
-  @layout = { menu: nil }
-  @num_players = 1
-  haml :main_page
-end
+  get '/' do
+    styles :application, :menus
+    scripts :application, :pong
+    @layout = { menu: 'menus/main' }
+    @num_players = 0
+    haml :main_page
+  end
 
-get '/multi' do
-  styles :application, :menus
-  scripts :application, :pong
-  @layout = { menu: nil }
-  @num_players = 2
-  haml :main_page
-end
+  get '/single' do
+    styles :application, :menus
+    scripts :application, :pong
+    @layout = { menu: nil }
+    @num_players = 1
+    haml :main_page
+  end
 
-get '/howto' do
-  styles :application, :menus
-  scripts :application, :pong
-  @layout = { menu: 'menus/howto' }
-  @num_players = 0
-  haml :main_page
+  get '/multi' do
+    styles :application, :menus
+    scripts :application, :pong
+    @layout = { menu: nil }
+    @num_players = 2
+    haml :main_page
+  end
+
+  get '/howto' do
+    styles :application, :menus
+    scripts :application, :pong
+    @layout = { menu: 'menus/howto' }
+    @num_players = 0
+    haml :main_page
+  end
+
+  # run the application if ruby file executed directly
+  run! if app_file == $0
 end
